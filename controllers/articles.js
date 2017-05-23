1b9710d7f4d4bae12b9a5f317236dc50e89c72ca
markdgold <mgoldstein@utexas.edu>
2017-05-22 21:14:44 -0700

    api calls moved to back-end. Can get sentiment for single newsAPI article

---
+++ controllers/articles.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+var newsApi = process.env.NEWSAPI;
+
+router.route('/:source')
+
+.get(function(req,res){
+		console.log('article control req')
+		request(`https://newsapi.org/v1/articles?source=${req.params.source}&sortBy=latest&apiKey=${newsApi}`, function(error, response, body){
+					if (error) console.log(error);
+					res.send(body);
+		});
+});
+module.exports = router;
